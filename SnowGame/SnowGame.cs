d3dd1bb5b6263982051295f05b27114552bff477
Badols Mito <badols@protonmail.com>
2022-07-17 19:07:40 -0300

    Added some basic stuff necessary for /join.

--- SnowGame/SnowGame.cs
+++ SnowGame/SnowGame.cs
@@ -32,7 +32,7 @@ namespace SnowGame
         
         public override void Initialize()
         {
-
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
         }
 
 
@@ -40,9 +40,59 @@ namespace SnowGame
         {
             if (disposing)
             {
-
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
             }
             base.Dispose(disposing);
         }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command(Join, "join"));
+        }
+
+        public enum TeamID
+        {
+            White,
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Pink
+        }
+
+
+
+        struct GameTeam
+        {
+            public TeamID color;
+            public int score;
+            public List<string> members;
+        }
+
+
+        GameTeam RedTeam = new GameTeam { color = TeamID.Red, score = 0 };
+        GameTeam BlueTeam = new GameTeam { color = TeamID.Blue, score = 0 };
+
+        public void Join(CommandArgs args)
+        {
+            if (RedTeam.members.Contains(args.Player.Name) || BlueTeam.members.Contains(args.Player.Name))
+            {
+                args.Player.SendInfoMessage("You're already in a team!");
+            }
+            else
+            {
+                // This should be RNG based but also check if there's an imbalance on
+                // the amount of members in the teams.
+                BlueTeam.members.Add(args.Player.Name);
+                //args.TPlayer.Teleport()
+            }
+        }
+
+        /*struct TeamMember
+        {
+            public ;
+            public int score;
+            public string name;
+        }*/
     }
 }
